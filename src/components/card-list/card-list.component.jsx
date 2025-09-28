31fb74aa23e1eb00449e23d3df484c934dd18815
Alex <alex@Marias-MacBook-Pro.local>
2025-09-28 00:08:26 -0400

    Finished 14-52

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import "./card-list.styles.css";
+import "./card.styles.css";
+import Card from "../card/card.component";
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { id, name, email } = monster;
+          return (
+            <Card
+              id={id}
+              name={name}
+              email={email}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
